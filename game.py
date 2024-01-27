f834d53aa87c8edba87fb3ca0beb35fab6d254e0
ciraben <tom.on.github@gmail.com>
2024-01-27 13:12:51 -0800

    Update window size. Subclass Window/View. Add framework for randomly generating next view.

--- game.py
+++ game.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 import arcade
+import random
 
-SCREEN_WIDTH, SCREEN_HEIGHT, TITLE = 350, 350, 'jester'
+
+SCREEN_WIDTH, SCREEN_HEIGHT, TITLE = 640, 360, 'jester'
 FONTSIZE = 24
 FONTNAME = 'monaco'
 
-class BaseView(arcade.View):
+class DevView(arcade.View):
+    def on_mouse_press(self, x, y, button, mods):
+        x_percent = int(x / SCREEN_WIDTH * 100)
+        y_percent = int(y / SCREEN_HEIGHT * 100)
+        print(f'{x}, {y} ({x_percent}%, {y_percent}%)')
+
+class BaseView(DevView):
+    def on_draw(self):
+        self.window.clear()
+    def on_key_press(self, key, mods):
+        if key == arcade.key.SPACE:
+            self.window.next_view()
+
+class TitleView(BaseView):
     def __init__(self):
         super().__init__()
         self.title = arcade.Text(
@@ -18,10 +33,38 @@ class BaseView(arcade.View):
         )
     def on_draw(self):
         self.title.draw()
+    def on_key_press(self, key, mods):
+        if key == arcade.key.SPACE:
+            self.window.next_view()
+
+class TestView1(BaseView):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.CHERRY_BLOSSOM_PINK)
+
+class TestView2(BaseView):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.CHROME_YELLOW)
+
+class TestView3(BaseView):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.CORNFLOWER_BLUE)
+
+class BaseWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
+        self.views = [] # excludes title
+        # add "speed" variable
+    def next_view(self):
+        NextViewClass = random.choice([TestView1, TestView2, TestView3])
+        _next_view = NextViewClass()
+        self.views.append(_next_view)
+        print(len(self.views))
+        self.show_view(_next_view)
+
 
 def main():
-    win = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
-    win.show_view(BaseView())
+    win = BaseWindow()
+    win.show_view(TitleView())
     arcade.run()
 
 if __name__ == '__main__':
