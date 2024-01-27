b9bc3f7b18fe02441e194835243edc02126eec30
ciraben <tom.on.github@gmail.com>
2024-01-27 00:33:20 -0800

    Create arcade window.

--- game.py
+++ game.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python3
 import arcade
-print('hi')
+
+SCREEN_WIDTH, SCREEN_HEIGHT, TITLE = 350, 350, 'jester'
+FONTSIZE = 24
+FONTNAME = 'monaco'
+
+class BaseView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.title = arcade.Text(
+            TITLE,
+            SCREEN_WIDTH * .5,
+            SCREEN_HEIGHT * .5,
+            font_name=FONTNAME,
+            font_size=FONTSIZE,
+            anchor_x='center'
+        )
+    def on_draw(self):
+        self.title.draw()
+
+def main():
+    win = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
+    win.show_view(BaseView())
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
